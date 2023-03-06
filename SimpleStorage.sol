c4c7b04fa24cd84c2ebb2ad42c648e96e6e6f6e5
Ansh <99464146+AnshMinchekar@users.noreply.github.com>
2023-03-06 18:44:23 +0530

    Create SimpleStorage.sol

---
+++ SimpleStorage.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+//every smart contract has an address 
+contract SimpleStorage{ 
+    //Data Types in Solidity
+    //uint: Unsigned Integer (Can only be positive values)
+    //int: Integer (Can have both +ve & -ve values)
+    //bool: Boolean values
+    //string (Are secretly bytes but only for text characters)
+    //address: Can hold wallet addresses 
+    //bytes: Can hold max. 32 byte data
+
+    uint256 public favNumber;
+    //Defaults to null or 0 when undefined 
+    //By default the visibility is set to private.
+
+    function store(uint256 _favNumber) public{
+        favNumber = _favNumber;
+    }
+
+    //Any time you change something on-chain (i.e, deploying a contract or fiddling with values, it happens in a transaction)
+}
